2cac952865dafa09f7fc74f8afff0f2e8084d1af
Fernando <touche.soul@gmail.com>
2015-06-20 20:12:58 +0100

    version sin funcionar las vistas, solo el index, necesita ser arreglado

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var partials= require('express-partials');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -6,15 +7,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -25,10 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);//instalacion de la ruta
 
 
-// catch 404 and forward to error handler
+// Manejo de Error de Ruta indefinida catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error(req.url);
   err.status = 404;
+  //res.render('404', { status: 404, url: req.url });// esto muestra el mensaje de error y la ruta donde se produce GENIAL!!
   next(err);
 });
 
@@ -41,7 +42,8 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
+      status: err.status
     });
   });
 }
@@ -58,3 +60,5 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+app.listen(80);
+console.log('Escuchando en el puerto 80');
