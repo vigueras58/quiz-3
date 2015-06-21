a1eec86428d7dc7780e6fc0c638faa957ee6b5ff
Fernando <touche.soul@gmail.com>
2015-06-21 09:12:17 +0100

    version que no funciona question por la etiqueta title de layout.ejs

--- app.js
+++ app.js
@@ -14,15 +14,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(partials());
+
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', routes);//instalacion de la ruta
+
 
 
 // Manejo de Error de Ruta indefinida catch 404 and forward to error handler
