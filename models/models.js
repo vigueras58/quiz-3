ebbe24fdb7434bbdd04da73296b6247e6d7081f1
Fernando <touche.soul@gmail.com>
2015-07-03 11:30:43 +0100

    fichero models modificado para elegir SQLite o postgres

--- models/models.js
+++ models/models.js
@@ -1,10 +1,29 @@
 var path = require('path');
 
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name 	=	(url[6] || null);
+var user 		=	(url[2] || null);
+var pwd 		=	(url[3] || null);
+var protocol 	= 	(url[1] || null);
+var dialect 	= 	(url[1] || null);
+var port 		=	(url[5] || null);
+var host 		=	(url[4] || null);
+var storage		=	process.env.DATABASE_STORAGE;
+
 //cargar modelo ORM 
 var Sequelize = require('sequelize');
 
+
 //user BBDD SQLite
-var sequelize = new Sequelize(null,null,null,{dialect:'sqlite',storage:'quiz.sqlite'});
+var sequelize = new Sequelize(DB_name,user,pwd,
+			{	
+				dialect:protocol,
+				protocol: protocol,
+				port: port,
+				host: host,
+				storage:storage,	//solo SQLite
+				omitNull: true		//solo Postgres
+				});
 
 //importar la definicion de la tabla quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
